61e8ec1c4dbe08bc40e03d7da5a15eaa72d40c47
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2023-10-30 15:55:33 -0400

    WT: Add settings for speed.
    * Add settings for speed of the wipe tower, both perimeters and infill.

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -625,6 +625,8 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionInt,                  infill_every_layers))
     ((ConfigOptionFloatOrPercent,       infill_overlap))
     ((ConfigOptionFloat,                infill_speed))
+    ((ConfigOptionFloat,                wipe_tower_perimeter_speed))
+    ((ConfigOptionFloat,                wipe_tower_infill_speed))
     // Ironing options
     ((ConfigOptionBool,                 ironing))
     ((ConfigOptionEnum<IroningType>,    ironing_type))

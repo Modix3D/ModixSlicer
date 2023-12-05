2f8c11ec4419d9f667a5211ffd3362b17b304055
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2023-12-04 20:43:35 -0500

    remove system send dialog.

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -93,7 +93,6 @@
 #include "SavePresetDialog.hpp"
 #include "PrintHostDialogs.hpp"
 #include "DesktopIntegrationDialog.hpp"
-#include "SendSystemInfoDialog.hpp"
 #include "Downloader.hpp"
 #include "PhysicalPrinterDialog.hpp"
 #include "WifiConfigDialog.hpp"
@@ -829,14 +828,8 @@ void GUI_App::post_init()
 #endif
         CallAfter([this] {
             // preset_updater->sync downloads profile updates on background so it must begin after config wizard finished.
-            bool cw_showed = this->config_wizard_startup();
+            this->config_wizard_startup();
             this->preset_updater->sync(preset_bundle, this);
-            if (! cw_showed) {
-                // The CallAfter is needed as well, without it, GL extensions did not show.
-                // Also, we only want to show this when the wizard does not, so the new user
-                // sees something else than "we want something" on the first start.
-                show_send_system_info_dialog_if_needed();   
-            }  
         });
     }
 

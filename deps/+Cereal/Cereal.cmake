4949b419d6287646331b0f57835d6e026cf95203
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2024-03-31 01:03:07 -0400

    deps: Cereal needs this patch for clang.

--- deps/+Cereal/Cereal.cmake
+++ deps/+Cereal/Cereal.cmake
@@ -1,6 +1,7 @@
 add_cmake_project(Cereal
     URL "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.2.zip"
     URL_HASH SHA256=e72c3fa8fe3d531247773e346e6824a4744cc6472a25cf9b30599cd52146e2ae
+    PATCH_COMMAND COMMAND ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/Cereal.patch
     CMAKE_ARGS
         -DJUST_INSTALL_CEREAL=ON
         -DSKIP_PERFORMANCE_COMPARISON=ON

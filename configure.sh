31a9146a89b5c22c2b6c2a756cd1a987f4c3f6cd
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2024-03-31 01:03:28 -0400

    update build script.

--- configure.sh
+++ configure.sh
@@ -12,11 +12,7 @@ if [ -f /usr/bin/ccache ]; then
 fi
 ARGS_STATIC="-DSLIC3R_STATIC=ON"
 ARGS="${ARGS_PREFIX_PATH} ${ARGS_CCACHE} ${ARGS_STATIC}"
-if [ -f /usr/bin/mold ]; then
-	ARGS="${ARGS} -DCMAKE_CXX_FLAGS=-fuse-ld=mold"
-	ARGS="${ARGS} -DCMAKE_C_FLAGS=-fuse-ld=mold"
-fi
 ARGS="${ARGS} -DCMAKE_COLOR_DIAGNOSTICS=ON"
 
-cmake ${ARGS} -DSLIC3R_ENABLE_FORMAT_STEP=false -B build-debug -DCMAKE_BUILD_TYPE=Debug &&
-cmake ${ARGS} -DSLIC3R_ENABLE_FORMAT_STEP=false -B build-release -DCMAKE_BUILD_TYPE=RelWithDebugInfo
+cmake ${ARGS} --preset no-occt -B build-debug -DCMAKE_BUILD_TYPE=Debug &&
+cmake ${ARGS} --preset no-occt -B build-release -DCMAKE_BUILD_TYPE=RelWithDebugInfo

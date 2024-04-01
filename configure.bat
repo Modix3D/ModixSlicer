7e878bbb6432f4b507171c5d2165e3fbdc6e709e
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2024-04-01 05:16:51 -0400

    update build script

--- configure.bat
+++ configure.bat
@@ -1,6 +1,6 @@
 REM This is probably only useful on my system...
 REM
 
-set "args=-DSLIC3R_STATIC=ON -DCMAKE_PREFIX_PATH=C:/src/PrusaSlicer-deps/usr/local -G Ninja"
+set "args=-DSLIC3R_STATIC=ON -DCMAKE_PREFIX_PATH=C:/src/ModixSlicer/deps/build/destdir/usr/local"
 
-cmake %args% --preset no-occt -B build-release -DCMAKE_BUILD_TYPE=Release
+cmake %args% --preset no-occt -Bbuild -H. -DCMAKE_BUILD_TYPE=Release -G "Ninja"
---
+++ deps/configure.bat
@@ -0,0 +1,4 @@
+REM This is probably only useful on my system...
+REM
+
+cmake --preset no-occt -B build -G "Ninja"

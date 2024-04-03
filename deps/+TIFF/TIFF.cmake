7ff87755335487710ab306ee543280795d0c956c
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2024-04-03 17:43:12 -0400

    build system: patch TIFF for mingw.

--- deps/+TIFF/TIFF.cmake
+++ deps/+TIFF/TIFF.cmake
@@ -1,6 +1,8 @@
 add_cmake_project(TIFF
-    URL https://gitlab.com/libtiff/libtiff/-/archive/v4.6.0/libtiff-v4.6.0.zip
-    URL_HASH SHA256=5d652432123223338a6ee642a6499d98ebc5a702f8a065571e1001d4c08c37e6
+    #URL https://gitlab.com/libtiff/libtiff/-/archive/v4.6.0/libtiff-v4.6.0.zip
+    #URL_HASH SHA256=5d652432123223338a6ee642a6499d98ebc5a702f8a065571e1001d4c08c37e6
+	GIT_REPOSITORY https://gitlab.com/libtiff/libtiff/
+	GIT_TAG cmake-cmath-avoid-imported-target
     CMAKE_ARGS
         -Dtiff-tools:BOOL=OFF
         -Dtiff-tests:BOOL=OFF
